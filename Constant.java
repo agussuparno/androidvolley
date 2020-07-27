f33be81a44b864ac855e35b5d0c6dc949f2fc33d
Agus Suparno <agussuparno@hotmail.com>
2020-07-27 17:50:55 +0700

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,18 @@
+package com.agussuparno.aplikasimahasiswa;
+
+public class Constant {
+    public static final String URL = "http://192.168.100.11/kampus/api.php";
+    public static final String accessKey ="access_key";
+    public static final String accessKeyValue = "123456";
+    public static final String getdata_mhs ="get_data_mahasiswa";
+    public static final String setdata_mhs ="set_data_mahasiswa";
+    public static final String editdata_mhs ="edit_data_mahasiswa";
+    public static final String deletedata_mhs ="delete_data_mahasiswa";
+    public static final String DATA = "data";
+    public static final String ERROR = "error";
+    public static final String id = "id";
+    public static final String nim = "nim";
+    public static final String nama = "nama";
+    public static final String telepon = "telepon";
+    public static final String email = "email";
+}
